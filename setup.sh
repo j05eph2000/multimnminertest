c7dec8fd17de7013b65b8a727478a43b99ccf1d0
j05eph2000 <47005688+j05eph2000@users.noreply.github.com>
2020-04-28 14:55:59 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -50,7 +50,7 @@ echo "/swapfile none swap sw 0 0" >> /etc/fstab
 
 fi
   #wget https://github.com/wagerr/wagerr/releases/download/v3.0.1/wagerr-3.0.1-x86_64-linux-gnu.tar.gz
-  wget https://github.com/Streamies/Streamies/releases/download/v2.3/Streamies-2.3.0.0-x86_64-pc-linux-gnu.zip
+  wget https://github.com/MINERIUM-COIN/minerium/releases/download/v3.0.0/Linux16.04-64bits.zip
   #wget https://github.com/wagerr/Wagerr-Blockchain-Snapshots/releases/download/Block-826819/826819.zip -O bootstrap.zip
   #export fileid=1cCOIb-Xqs_MxsIPiwEAtvQps_6R1Ww13
   #export filename=Streamies-2.0.0-x86_64-pc-linux-gnu.zip
@@ -67,17 +67,17 @@ fi
 
   #wget --load-cookies cookies.txt -O $filename \
   #   'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
-  unzip Streamies-2.3.0.0-x86_64-pc-linux-gnu.zip
+  unzip Linux16.04-64bits.zip
   
-  chmod +x streamiesd
-  chmod +x streamies-cli
-  sudo cp  streamiesd /usr/local/bin
-  sudo cp  streamies-cli /usr/local/bin
-  rm -rf Streamies-2.3.0.0-x86_64-pc-linux-gnu.zip
-  rm -rf streamies-cli
-  rm -rf streamiesd
-  rm -rf streamies-tx
-  rm -rf streamies-qt
+  chmod +x mineriumd
+  chmod +x minerium-cli
+  sudo cp  mineriumd /usr/local/bin
+  sudo cp  minerium-cli /usr/local/bin
+  rm -rf Linux16.04-64bits.zip
+  rm -rf minerium-cli
+  rm -rf mineriumd
+  
+  rm -rf minerium-qt
   
   sudo apt install -y ufw
   sudo ufw allow ssh/tcp
@@ -123,58 +123,49 @@ for i in `seq 1 1 $MNCOUNT`; do
   CONF_DIR=~/.streamies_$ALIAS
 
   # Create scripts
-  echo '#!/bin/bash' > ~/bin/streamiesd_$ALIAS.sh
-  echo "streamiesd -daemon -conf=$CONF_DIR/streamies.conf -datadir=$CONF_DIR "'$*' >> ~/bin/streamiesd_$ALIAS.sh
-  echo '#!/bin/bash' > ~/bin/streamies-cli_$ALIAS.sh
-  echo "streamies-cli -conf=$CONF_DIR/streamies.conf -datadir=$CONF_DIR "'$*' >> ~/bin/streamies-cli_$ALIAS.sh
-  echo '#!/bin/bash' > ~/bin/streamies-tx_$ALIAS.sh
-  echo "streamies-tx -conf=$CONF_DIR/streamies.conf -datadir=$CONF_DIR "'$*' >> ~/bin/streamies-tx_$ALIAS.sh 
-  chmod 755 ~/bin/streamies*.sh
+  echo '#!/bin/bash' > ~/bin/mineriumd_$ALIAS.sh
+  echo "mineriumd -daemon -conf=$CONF_DIR/minerium.conf -datadir=$CONF_DIR "'$*' >> ~/bin/minerium_$ALIAS.sh
+  echo '#!/bin/bash' > ~/bin/minerium-cli_$ALIAS.sh
+  echo "minerium-cli -conf=$CONF_DIR/minerium.conf -datadir=$CONF_DIR "'$*' >> ~/bin/minerium-cli_$ALIAS.sh
+  
+  chmod 755 ~/bin/minerium*.sh
 
   mkdir -p $CONF_DIR
   #unzip  bootstrap.zip -d $CONF_DIR
-  echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> streamies.conf_TEMP
-  echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> streamies.conf_TEMP
-  echo "rpcallowip=127.0.0.1" >> streamies.conf_TEMP
-  echo "rpcport=$RPCPORT" >> streamies.conf_TEMP
-  echo "listen=1" >> streamies.conf_TEMP
-  echo "server=1" >> streamies.conf_TEMP
-  echo "daemon=1" >> streamies.conf_TEMP
-  echo "logtimestamps=1" >> streamies.conf_TEMP
-  echo "maxconnections=256" >> streamies.conf_TEMP
-  echo "masternode=1" >> streamies.conf_TEMP
-  echo "" >> streamies.conf_TEMP
+  echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> minerium.conf_TEMP
+  echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> minerium.conf_TEMP
+  echo "rpcallowip=127.0.0.1" >> minerium.conf_TEMP
+  echo "rpcport=$RPCPORT" >> minerium.conf_TEMP
+  echo "listen=1" >> minerium.conf_TEMP
+  echo "server=1" >> minerium.conf_TEMP
+  echo "daemon=1" >> minerium.conf_TEMP
+  echo "logtimestamps=1" >> minerium.conf_TEMP
+  echo "maxconnections=256" >> minerium.conf_TEMP
+  echo "masternode=1" >> minerium.conf_TEMP
+  echo "" >> minerium.conf_TEMP
 
-  echo "" >> streamies.conf_TEMP
-  echo "port=$PORT" >> streamies.conf_TEMP
-  echo "masternodeaddr=$IP:55297" >> streamies.conf_TEMP
-  echo "masternodeprivkey=$PRIVKEY" >> streamies.conf_TEMP
-  echo "addnode=172.105.62.134" >> streamies.conf_TEMP
-  echo "addnode=172.86.75.178" >> streamies.conf_TEMP
-  echo "addnode=172.86.75.16" >> streamies.conf_TEMP
-  echo "addnode=172.86.75.176" >> streamies.conf_TEMP
-  echo "addnode=45.76.83.165:55297" >> streamies.conf_TEMP
-  echo "addnode=145.129.129.109:55297" >> streamies.conf_TEMP
-  echo "addnode=174.86.28.8:55297" >> streamies.conf_TEMP
-  echo "addnode=95.179.246.187:55297" >> streamies.conf_TEMP
-
+  echo "" >> minerium.conf_TEMP
+  echo "port=$PORT" >> minerium.conf_TEMP
+  echo "masternodeaddr=$IP:6116" >> minerium.conf_TEMP
+  echo "masternodeprivkey=$PRIVKEY" >> minerium.conf_TEMP
+  
   
   sudo ufw allow $PORT/tcp
 
-  mv streamies.conf_TEMP $CONF_DIR/streamies.conf
+  mv minerium.conf_TEMP $CONF_DIR/minerium.conf
   
   #sh ~/bin/iond_$ALIAS.sh
   
-  cat << EOF > /etc/systemd/system/streamies_$ALIAS.service
+  cat << EOF > /etc/systemd/system/minerium_$ALIAS.service
 [Unit]
-Description=streamies_$ALIAS service
+Description=minerium_$ALIAS service
 After=network.target
 [Service]
 User=root
 Group=root
 Type=forking
-ExecStart=/usr/local/bin/streamiesd -daemon -conf=$CONF_DIR/streamies.conf -datadir=$CONF_DIR
-ExecStop=/usr/local/bin/streamies-cli -conf=$CONF_DIR/streamies.conf -datadir=$CONF_DIR stop
+ExecStart=/usr/local/bin/mineriumd -daemon -conf=$CONF_DIR/minerium.conf -datadir=$CONF_DIR
+ExecStop=/usr/local/bin/minerium-cli -conf=$CONF_DIR/minerium.conf -datadir=$CONF_DIR stop
 Restart=always
 PrivateTmp=true
 TimeoutStartSec=10m
@@ -185,8 +176,8 @@ EOF
 
   systemctl daemon-reload
   sleep 10
-  systemctl start streamies_$ALIAS.service
-  systemctl enable streamies_$ALIAS.service >/dev/null 2>&1
+  systemctl start minerium_$ALIAS.service
+  systemctl enable minerium_$ALIAS.service >/dev/null 2>&1
  
   rm -rf setup.sh
 
